1425a0d7c7fdd05b20e6a5a06691080e9c7cd222
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-04-24 17:00:16 +0530

    Second commit

---
+++ Ascii.py
@@ -0,0 +1,11 @@
+#Char to Ascii
+char='A'
+ascii_code=ord(char)
+
+print(f"The Asciii code of '{char}'is {ascii_code}")
+
+#Ascii to Char
+ascii_code=97
+character=chgit r(ascii_code)
+
+print(f"The character with Ascii code '{ascii_code}'is {character}")
--- Char to Ascii.py
+++
@@ -1,4 +0,0 @@
-char='A'
-ascii_code=ord(char)
-
-print(f"The Asciii code of '{char}'is {ascii_code}")
